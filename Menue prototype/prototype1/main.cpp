2f04effb7d82ab82eec040dffd9916aa7c210738
Alex Modee <alex.modee@elev.ga.klaragymnasium.se>
2016-12-01 20:57:53 +0100

    New version of menue
    AS far as can tell working menue, quit option works but no othere
    options are "selectable"

--- Menue prototype/prototype1/main.cpp
+++ Menue prototype/prototype1/main.cpp
@@ -20,14 +20,14 @@ int main() {
 	sf::Font font;
 	if (!font.loadFromFile("ALGER.ttf"))
 	{
-		return 0;
+		std::cout << "font not loaded";
 	}
 
 	Text title;
 	title.setFont(font);
 	title.setString("Dear Isaac");
 	title.setCharacterSize(60);
-	title.setColor(white);
+	title.setFillColor(white);
 	title.setPosition(Vector2f(540 - title.getLocalBounds().width*0.5, 200));
 
 	int header_pos_y[3] = { 400,500,600 };
@@ -38,27 +38,29 @@ int main() {
 	option_header[2].setString("QUIT");
 	for (int i = 0; i <= 2; i++) {
 		option_header[i].setFont(font);
-		option_header[i].setColor(white);
+		option_header[i].setFillColor(white);
 		option_header[i].setCharacterSize(30);
 		option_header[i].setPosition(Vector2f(540 - option_header[i].getLocalBounds().width*0.5, header_pos_y[i]));
 	}
 
 
-	RectangleShape underline(Vector2f(option_header[0].getLocalBounds().width * 2, 2));
+	RectangleShape underline(Vector2f(option_header[0].getLocalBounds().width * 1.5, 2));
 	underline.setFillColor(white);
-	underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, header_pos_y[0] + option_header[0].getLocalBounds().height*1.3));
+	underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, option_header[0].getPosition().y + option_header[0].getLocalBounds().height*1.3));
 
 	bool selected[3] = { 0,0,0 };
-
+	std::cout << underline.getPosition().y << std::endl << option_header[0].getPosition().y + option_header[0].getLocalBounds().height*1.3;
 	while (window.isOpen()) {
 		Event input;
 
 		for (int i = 0; i <= 2; i++) {
-			if (underline.getPosition().y - option_header[i].getLocalBounds().height*1.3 != option_header[i].getPosition().y) {
-				selected[i] = 0;
-			}
-		 if (underline.getPosition().y - option_header[i].getLocalBounds().height*1.3 == option_header[i].getPosition().y) {
+			float activ_header_poss = option_header[i].getPosition().y + option_header[i].getLocalBounds().height*1.3;
+			if (underline.getPosition().y == activ_header_poss) {
 				selected[i] = 1;
+			}
+			if (underline.getPosition().y != activ_header_poss) {
+				selected[i] = 0;
+
 			}
 		}
 
@@ -66,14 +68,15 @@ int main() {
 
 			if ((input.type == Event::KeyPressed) && (input.key.code == Keyboard::Down)) {
 				for (int i = 0; i <= 2; i++) {
-					if (selected[i]==1) {
+					if (selected[i] == 1) {
 						if (i <= 1) {
-			underline.setSize(Vector2f(option_header[i + 1].getLocalBounds().width * 2, 2));
-							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, header_pos_y[i+1] + option_header[i+1].getLocalBounds().height*1.3));
+							underline.setSize(Vector2f(option_header[i + 1].getLocalBounds().width * 1.5, 2));
+							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, option_header[i + 1].getPosition().y + option_header[i + 1].getLocalBounds().height*1.3));
 							break;
-						}else {
-		underline.setSize(Vector2f(option_header[0].getLocalBounds().width * 2, 2));
-							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, header_pos_y[0] + option_header[0].getLocalBounds().height*1.3));
+						}
+						else {
+							underline.setSize(Vector2f(option_header[0].getLocalBounds().width * 1.5, 2));
+							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, option_header[0].getPosition().y + option_header[0].getLocalBounds().height*1.3));
 							break;
 						}
 					}
@@ -82,18 +85,24 @@ int main() {
 
 			if ((input.type == Event::KeyPressed) && (input.key.code == Keyboard::Up)) {
 				for (int i = 0; i <= 2; i++) {
-					if (selected[i]==1) {
+					if (selected[i] == 1) {
 						if (i >= 1) {
-				underline.setSize(Vector2f(option_header[i - 1].getLocalBounds().width * 2, 2));
-							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, header_pos_y[i-1] + option_header[i-1].getLocalBounds().height*1.3));
+							underline.setSize(Vector2f(option_header[i - 1].getLocalBounds().width * 1.5, 2));
+							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, header_pos_y[i - 1] + option_header[i - 1].getLocalBounds().height*1.3));
 							break;
 						}
 						else {
-					underline.setSize(Vector2f(option_header[2].getLocalBounds().width * 2, 2));
-							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5,header_pos_y[2] + option_header[2].getLocalBounds().height*1.3));
+							underline.setSize(Vector2f(option_header[2].getLocalBounds().width * 1.5, 2));
+							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, header_pos_y[2] + option_header[2].getLocalBounds().height*1.3));
 							break;
 						}
 					}
+				}
+			}
+
+			if ((input.type == Event::KeyPressed) && (input.key.code != Keyboard::Up) && (input.key.code != Keyboard::Down)) {
+				if (selected[2] == 1) {
+					return 0;
 				}
 			}
 
