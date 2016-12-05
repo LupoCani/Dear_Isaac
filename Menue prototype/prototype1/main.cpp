31514a61b0d354f573536b1d3dfe88da47fb97bd
ModeSten <alex.modee@gmail.com>
2016-12-05 14:53:32 +0100

    Uppdate to start menue
    Added shell for option menue

--- Menue prototype/prototype1/main.cpp
+++ Menue prototype/prototype1/main.cpp
@@ -7,17 +7,106 @@
 #include <windows.h>
 
 using namespace sf;
-
+sf::Font font;
 sf::RenderWindow window(sf::VideoMode(1080, 860), "Orbitals");
 Color yellow(255, 250, 7);
 Color Red(165, 41, 13);
 Color grey(177, 190, 198);
 Color white(200, 200, 200);
 
+void option_menue(){
+
+	int header_pos_y[3] = { 300,400 };
+	Text option_header[2];
+	option_header[0].setString("Sound");
+	option_header[1].setString("BACK");
+	for (int i = 0; i <= 1; i++) {
+		option_header[i].setFont(font);
+		option_header[i].setFillColor(white);
+		option_header[i].setCharacterSize(30);
+		option_header[i].setPosition(Vector2f(540 - option_header[i].getLocalBounds().width*0.5, header_pos_y[i]));
+	}
+
+
+	RectangleShape underline(Vector2f(option_header[0].getLocalBounds().width * 1.5, 2));
+	underline.setFillColor(white);
+	underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, option_header[0].getPosition().y + option_header[0].getLocalBounds().height*1.3));
+	bool run_option = 1;
+	bool selected[2] = { 0,0};
+
+	while (run_option==1) {
+		Event input;
+
+		for (int i = 0; i <= 1; i++) {
+			float activ_header_poss = option_header[i].getPosition().y + option_header[i].getLocalBounds().height*1.3;
+			if (underline.getPosition().y == activ_header_poss) {
+				selected[i] = 1;
+			}
+			if (underline.getPosition().y != activ_header_poss) {
+				selected[i] = 0;
+			}
+		}
+
+		while (window.pollEvent(input)) {
+
+			if ((input.type == Event::KeyPressed) && (input.key.code == Keyboard::Down)) {
+				for (int i = 0; i <= 1; i++) {
+					if (selected[i] == 1) {
+						if (i == 0) {
+							underline.setSize(Vector2f(option_header[i + 1].getLocalBounds().width * 1.5, 2));
+							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, option_header[i + 1].getPosition().y + option_header[i + 1].getLocalBounds().height*1.3));
+							break;
+						}
+						else {
+							underline.setSize(Vector2f(option_header[0].getLocalBounds().width * 1.5, 2));
+							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, option_header[0].getPosition().y + option_header[0].getLocalBounds().height*1.3));
+							break;
+						}
+					}
+				}
+			}
+
+			if ((input.type == Event::KeyPressed) && (input.key.code == Keyboard::Up)) {
+				for (int i = 0; i <= 1; i++) {
+					if (selected[i] == 1) {
+						if (i == 1) {
+							underline.setSize(Vector2f(option_header[i - 1].getLocalBounds().width * 1.5, 2));
+							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, header_pos_y[i - 1] + option_header[i - 1].getLocalBounds().height*1.3));
+							break;
+						}
+						else {
+							underline.setSize(Vector2f(option_header[1].getLocalBounds().width * 1.5, 2));
+							underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, header_pos_y[1] + option_header[1].getLocalBounds().height*1.3));
+							break;
+						}
+					}
+				}
+			}
+
+			if ((input.type == Event::KeyPressed) && (input.key.code != Keyboard::Up) && (input.key.code != Keyboard::Down)) {
+				if (selected[1] == 1) {
+					run_option = 0;
+					break;
+				}
+			}
+
+		}
+
+		window.clear();
+		for (int i = 0; i <= 1; i++) {
+			window.draw(option_header[i]);
+		}
+		window.draw(underline);
+		window.display();
+
+	}
+
+}
+
 
 int main() {
 
-	sf::Font font;
+
 	if (!font.loadFromFile("ALGER.ttf"))
 	{
 		std::cout << "font not loaded";
@@ -49,7 +138,7 @@ int main() {
 	underline.setPosition(Vector2f(540 - underline.getSize().x*0.5, option_header[0].getPosition().y + option_header[0].getLocalBounds().height*1.3));
 
 	bool selected[3] = { 0,0,0 };
-	std::cout << underline.getPosition().y << std::endl << option_header[0].getPosition().y + option_header[0].getLocalBounds().height*1.3;
+	
 	while (window.isOpen()) {
 		Event input;
 
@@ -103,6 +192,9 @@ int main() {
 			if ((input.type == Event::KeyPressed) && (input.key.code != Keyboard::Up) && (input.key.code != Keyboard::Down)) {
 				if (selected[2] == 1) {
 					return 0;
+				}
+				else if (selected[1] == 1) {
+					option_menue();
 				}
 			}
 
